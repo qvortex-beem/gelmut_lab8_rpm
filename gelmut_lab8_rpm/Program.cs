acbfacd2fb2a3509f7189b5bd9c62791b1640b58
1 <1>
2025-10-13 13:32:02 +0300

    Add project files.

---
+++ gelmut_lab8_rpm/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace gelmut_lab8_rpm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class AirBalloon
+    {
+        double volume;
+        double mass;
+        double molar;
+
+        double GetPressure(int t) { return 0.0; }
+        double AmountOfMatter() { return 0.0; }
+        string ToString() { return ""; }
+    }
+
+    interface ITarget
+    {
+        void CalculateDp(int t0, int dt);
+        void ModifMass(double dm);
+        void GetData();
+    }
+
+    class Adapter : AirBalloon, ITarget 
+    {     
+
+    }
+}

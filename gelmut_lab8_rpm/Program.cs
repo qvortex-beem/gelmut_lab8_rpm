daebd644a2b3fa38290be968ec9e5844db53966a
A-39GFp-78 <A-39GFp-78@DESKTOP-76J6RFA>
2025-10-23 17:40:38 +0300

    выполнил все, кроме схемы

--- gelmut_lab8_rpm/Program.cs
+++ gelmut_lab8_rpm/Program.cs
@@ -1,33 +1,61 @@
-﻿namespace gelmut_lab8_rpm
+﻿using System.Drawing;
+
+namespace gelmut_lab8_rpm
 {
     class Program
     {
         static void Main(string[] args)
         {
+            AirBalloon obj = new AirBalloon();
+            obj.molar = 0.032;
+            obj.volume = 50;
+            obj.mass = 34;
+            ITarget target = new Adapter(obj);
+            Console.WriteLine(target.GetData()); 
+            target.ModifMass(6); 
+            Console.WriteLine(target.GetData());
+            Console.WriteLine($"Изменение давления при заданной T0 и изменнении температуры dT: {target.CalculateDp(15, 10)}");
+
 
         }
     }
 
     class AirBalloon
     {
-        double volume;
-        double mass;
-        double molar;
+        public double volume { get; set; }
+        public double mass  {get; set; }
+        public double molar  {get; set; }
 
-        double GetPressure(int t) { return 0.0; }
-        double AmountOfMatter() { return 0.0; }
-        string ToString() { return ""; }
+        public double GetPressure(int t) { return (AmountOfMatter() * 8.314 * t) / volume; }
+        public double AmountOfMatter() { return mass / molar; }
+        public string ToString() { return $"{volume} {mass} {molar}"; }
     }
 
     interface ITarget
     {
-        void CalculateDp(int t0, int dt);
+        double CalculateDp(int t0, int dt);
         void ModifMass(double dm);
-        void GetData();
+        string GetData();
     }
 
-    class Adapter : AirBalloon, ITarget 
-    {     
-
+    class Adapter : AirBalloon, ITarget
+    {
+        private AirBalloon obj { get; set; }
+        public Adapter(AirBalloon obj)
+        {
+            this.obj = obj;
+        }
+        public double CalculateDp(int T0, int dT)
+        {
+            return ((obj.AmountOfMatter() * 8.314 * (T0 + dT) / obj.volume) - obj.GetPressure(T0));
+        }
+        public void ModifMass(double dm)
+        {
+            obj.mass += dm;
+        }
+        public string GetData()
+        {
+            return obj.ToString();
+        }
     }
 }
--- gelmut_lab8_rpm/example.cs
+++ gelmut_lab8_rpm/example.cs
@@ -1,107 +1,107 @@
-﻿// Паттерн Адаптер для объекта. Решение на языке C#
-using static System.Console;
+﻿//// Паттерн Адаптер для объекта. Решение на языке C#
+//using static System.Console;
 
-namespace ConsoleApp10
-{
-    // Исходный класс Original, который содержит 3 метода
-    // OriginalDouble(), OriginalInt(), OriginalChar()
-    class Original
-    {
-        public void OriginalDouble(double value)
-        {
-            WriteLine("Method Original.OriginalDouble(), value = {0}", value);
-        }
+//namespace ConsoleApp10
+//{
+//    // Исходный класс Original, который содержит 3 метода
+//    // OriginalDouble(), OriginalInt(), OriginalChar()
+//    class Original
+//    {
+//        public void OriginalDouble(double value)
+//        {
+//            WriteLine("Method Original.OriginalDouble(), value = {0}", value);
+//        }
 
-        public void OriginalInt(int value)
-        {
-            WriteLine("Method Original.OriginalInt(), value = {0}", value);
-        }
+//        public void OriginalInt(int value)
+//        {
+//            WriteLine("Method Original.OriginalInt(), value = {0}", value);
+//        }
 
-        public void OriginalChar(char value)
-        {
-            WriteLine("Method Original.OriginalChar, value = {0}", value);
-        }
-    }
+//        public void OriginalChar(char value)
+//        {
+//            WriteLine("Method Original.OriginalChar, value = {0}", value);
+//        }
+//    }
 
-    // Объявить интерфейс ITarget,
-    // который содержит названия методов, которые нужны для клиента
-    interface ITarget
-    {
-        void ClientDouble(double value);
-        void ClientInt(int value);
-        void ClientChar(char value);
-    }
+//    // Объявить интерфейс ITarget,
+//    // который содержит названия методов, которые нужны для клиента
+//    interface ITarget
+//    {
+//        void ClientDouble(double value);
+//        void ClientInt(int value);
+//        void ClientChar(char value);
+//    }
 
-    // Класс Adapter - реализует интерфейс ITarget.
-    // Класс получает входящим параметром ссылку на Original в конструкторе.
-    class Adapter : Original, ITarget
-    {
-        // Ссылка на класс, который нужно адаптировать
-        private Original obj;
+//    // Класс Adapter - реализует интерфейс ITarget.
+//    // Класс получает входящим параметром ссылку на Original в конструкторе.
+//    class Adapter : Original, ITarget
+//    {
+//        // Ссылка на класс, который нужно адаптировать
+//        private Original obj;
 
-        // Конструктор класса
-        public Adapter(Original obj)
-        {
-            this.obj = obj;
-        }
+//        // Конструктор класса
+//        public Adapter(Original obj)
+//        {
+//            this.obj = obj;
+//        }
 
-        public void ClientDouble(double value)
-        {
-            // в середине метода ClientDouble() вызвать метод OriginalDouble()
-            obj.OriginalDouble(value);
-        }
+//        public void ClientDouble(double value)
+//        {
+//            // в середине метода ClientDouble() вызвать метод OriginalDouble()
+//            obj.OriginalDouble(value);
+//        }
 
-        public void ClientInt(int value)
-        {
-            obj.OriginalInt(value * 2);
-        }
+//        public void ClientInt(int value)
+//        {
+//            obj.OriginalInt(value * 2);
+//        }
 
-        public void ClientChar(char value)
-        {
-            for (int i = 0; i < 5; i++)
-                obj.OriginalChar(value);
-        }
-    }
+//        public void ClientChar(char value)
+//        {
+//            for (int i = 0; i < 5; i++)
+//                obj.OriginalChar(value);
+//        }
+//    }
 
-    // Класс Client - получает ссылку на интерфейс ITarget в конструкторе
-    class Client
-    {
-        private ITarget client; // ссылка на интерфейс ITarget
+//    // Класс Client - получает ссылку на интерфейс ITarget в конструкторе
+//    class Client
+//    {
+//        private ITarget client; // ссылка на интерфейс ITarget
 
-        // Конструктор
-        public Client(ITarget _client)
-        {
-            client = _client;
-        }
+//        // Конструктор
+//        public Client(ITarget _client)
+//        {
+//            client = _client;
+//        }
 
-        // Метод, который вызывает все методы интерфейса
-        public void Show()
-        {
-            client.ClientDouble(2.88);
-            client.ClientInt(39);
-            client.ClientChar('f');
-        }
-    }
+//        // Метод, который вызывает все методы интерфейса
+//        public void Show()
+//        {
+//            client.ClientDouble(2.88);
+//            client.ClientInt(39);
+//            client.ClientChar('f');
+//        }
+//    }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Демонстрация паттерна Adapter для класса.
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Демонстрация паттерна Adapter для класса.
 
-            // 1. Объявить экземпляр класса Original
-            Original obj = new Original();
+//            // 1. Объявить экземпляр класса Original
+//            Original obj = new Original();
 
-            // 2. Объявить экземпляр класса Adapter, передать ему obj
-            Adapter adapter = new Adapter(obj);
+//            // 2. Объявить экземпляр класса Adapter, передать ему obj
+//            Adapter adapter = new Adapter(obj);
 
-            // 3. Объявить экземпляр класса Client и передать ему экземпляр адаптера
-            Client client = new Client(adapter);
+//            // 3. Объявить экземпляр класса Client и передать ему экземпляр адаптера
+//            Client client = new Client(adapter);
 
-            // 4. Вызвать метод Show() клиента
-            client.Show();
+//            // 4. Вызвать метод Show() клиента
+//            client.Show();
 
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.ReadKey();
+//        }
+//    }
+//}
